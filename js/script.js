e0e508e54d5e6130ec654dd521ea7e527bde635a
CdotW <cwhiting428@gmail.com>
2021-06-16 17:07:38 -0400

    hide the other job role element

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Test');
+const name = document.getElementById('name');
+name.focus();
+
+const jobRole = document.querySelector('#title');
+const otherJobRole = document.getElementById('other-job-role');
+jobRole.addEventListener('change', (e) => {
+    if (e.target.value === 'other') {
+        otherJobRole.style.visibility = 'visible';
+    } else {
+        otherJobRole.style.visibility = 'hidden';
+    };
+});
